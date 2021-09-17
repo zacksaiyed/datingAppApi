aadafda3a5185819a88fbeae95291424739535b8
Zaid Saiyed <zaid.saiyed@technovate.in>
2021-09-17 22:31:40 +0530

    Dating App 1 commit

---
+++ datingAppApi/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using datingAppApi.Data;
+using datingAppApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingAppApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            this._context = context;
+        }
+        
+        [HttpPost("GetUsers")]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+                
+            return users;
+        }
+
+        [HttpPost("GetUser")]
+        public async Task<ActionResult<AppUser>> GetUser([FromBody]int id)
+        {
+            var users =  await _context.Users.FindAsync(id);
+
+            return users;
+        }
+
+    }
+}
